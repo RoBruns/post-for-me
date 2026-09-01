eb3b1cfb6450a84e5ce2a10cea127d92bdcf1a03
Rodrigo Bruns <117675904+RoBruns@users.noreply.github.com>
2026-09-01 16:04:41 -0300

    Avoid requiring Stripe credentials in self-hosted mode

--- dashboard/app/lib/.server/stripe.ts
+++ dashboard/app/lib/.server/stripe.ts
@@ -2,6 +2,7 @@ import Stripe from "stripe";
 
 import { STRIPE_SECRET_KEY } from "./stripe.constants";
 
-export const stripe = new Stripe(STRIPE_SECRET_KEY, {
-  typescript: true,
-});
+export const stripe = new Stripe(
+  STRIPE_SECRET_KEY || "sk_self_hosted_disabled",
+  { typescript: true },
+);
