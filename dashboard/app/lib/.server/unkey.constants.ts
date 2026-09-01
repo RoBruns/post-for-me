c73a33b04a56bf9486f6955ef2e99fcc910a985f
Rodrigo Bruns <117675904+RoBruns@users.noreply.github.com>
2026-09-01 16:04:09 -0300

    Make Unkey optional when self-hosted

--- dashboard/app/lib/.server/unkey.constants.ts
+++ dashboard/app/lib/.server/unkey.constants.ts
@@ -1,13 +1,15 @@
 import type { RatelimitRequest } from "@unkey/api/models/components";
 
+const SELF_HOSTED = process.env.SELF_HOSTED === "true";
+
 export const UNKEY_ROOT_KEY = process.env?.UNKEY_ROOT_KEY || "";
 export const UNKEY_API_ID = process.env?.UNKEY_API_ID || "";
 
-if (!UNKEY_ROOT_KEY || UNKEY_ROOT_KEY.trim() === "") {
+if (!SELF_HOSTED && (!UNKEY_ROOT_KEY || UNKEY_ROOT_KEY.trim() === "")) {
   throw new Error("UNKEY_ROOT_KEY is not defined");
 }
 
-if (!UNKEY_API_ID || UNKEY_API_ID.trim() === "") {
+if (!SELF_HOSTED && (!UNKEY_API_ID || UNKEY_API_ID.trim() === "")) {
   throw new Error("UNKEY_API_ID is not defined");
 }
 
